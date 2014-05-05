bcb3d8b4f55fa09ee56f92bdbb0fd7c251dfaa50
Brad Kammin <bkammin@nypublicradio.org>
2014-05-05 16:20:43 -0400

    fire alarm time at exact time, launch main activity

--- src/android/WakeupPlugin.java
+++ src/android/WakeupPlugin.java
@@ -21,44 +21,45 @@ import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 public class WakeupPlugin extends CordovaPlugin {
-	
+
 	protected static final String LOG_TAG = "WakeupPlugin";
 
-	protected static  Map<String , Integer> daysOfWeek = new HashMap<String , Integer>() {
+	public static  Map<String , Integer> daysOfWeek = new HashMap<String , Integer>() {
 		private static final long serialVersionUID = 1L;
 		{
-		    put("sunday", 0);
-		    put("monday", 1);
-		    put("tuesday", 2);
-		    put("wednesday", 3);
-		    put("thursday", 4);
-		    put("friday", 5);
-		    put("saturday", 6);
+			put("sunday", 0);
+			put("monday", 1);
+			put("tuesday", 2);
+			put("wednesday", 3);
+			put("thursday", 4);
+			put("friday", 5);
+			put("saturday", 6);
 		}
 	};
-	
+
 	protected boolean _countdown;
 	protected Timer _countdownTimer;
 	protected long _time;
 	public static CallbackContext connectionCallbackContext;
-	
+
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		boolean ret=true;
 		try {
 			if(action.equalsIgnoreCase("wakeup")) {
 				JSONObject options=args.getJSONObject(0);
-				
+
 				setAlarms(options.getJSONArray("alarms"));
-				
+
 				WakeupPlugin.connectionCallbackContext = callbackContext;
-	            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-	            pluginResult.setKeepCallback(true);
-	            callbackContext.sendPluginResult(pluginResult);  
-			
+				PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+				pluginResult.setKeepCallback(true);
+				callbackContext.sendPluginResult(pluginResult);  
+
 			}else{
 				callbackContext.error(LOG_TAG + " error: invalid action (" + action + ")");
 				ret=false;
@@ -73,90 +74,95 @@ public class WakeupPlugin extends CordovaPlugin {
 		return ret;
 	}
 
-@SuppressLint("SimpleDateFormat")
-protected void setAlarms(JSONArray alarms) throws JSONException{
-	  
-	  cancelAlarms();
-	
-	  for(int i=0;i<alarms.length();i++){
-		  JSONObject alarm=alarms.getJSONObject(i);
-		  if (!alarm.has("time") || !alarm.has("days")){
-			  throw new JSONException("Invalid alarm configuration: " + alarm.toString());
-		  }
-		  JSONArray days=alarm.getJSONArray("days");
-		  JSONObject time=alarm.getJSONObject("time");
-		  for (int j=0;j<days.length();j++){   
-				  Calendar alarmDate=getAlarmDate(time, daysOfWeek.get(days.getString(j)));
-				  if(alarmDate!=null){
-					  SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			          Log.d(LOG_TAG,"setting alarm at " + sdf.format(alarmDate.getTime()));
-			          
-			          Intent intent = new Intent(cordova.getActivity().getApplicationContext() , WakeupReceiver.class);
-			          if(alarm.has("extra")){
-			        	  intent.putExtra("extra", alarm.getJSONObject("extra").toString());
-			          }
-			          
-			          PendingIntent sender = PendingIntent.getBroadcast(cordova.getActivity().getApplicationContext(), 19999 + j, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-			          AlarmManager alarmManager = (AlarmManager) cordova.getActivity().getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-			          alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, alarmDate.getTimeInMillis(), 1000*60*60*24*7, sender); // repeat weekly
-				 }
-		  }
-	  }
-  }
+	@SuppressLint({ "SimpleDateFormat", "NewApi" })
+	protected void setAlarms(JSONArray alarms) throws JSONException{
+
+		cancelAlarms();
+
+		for(int i=0;i<alarms.length();i++){
+			JSONObject alarm=alarms.getJSONObject(i);
+			if (!alarm.has("time") || !alarm.has("days")){
+				throw new JSONException("Invalid alarm configuration: " + alarm.toString());
+			}
+			JSONArray days=alarm.getJSONArray("days");
+			JSONObject time=alarm.getJSONObject("time");
+			for (int j=0;j<days.length();j++){
+				Calendar alarmDate=getAlarmDate(time, daysOfWeek.get(days.getString(j)));
+				if(alarmDate!=null){
+					SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+					Log.d(LOG_TAG,"setting alarm at " + sdf.format(alarmDate.getTime()));
+
+					Intent intent = new Intent(cordova.getActivity().getApplicationContext() , WakeupReceiver.class);
+					if(alarm.has("extra")){
+						intent.putExtra("extra", alarm.getJSONObject("extra").toString());
+						intent.putExtra("time", time.toString());
+						intent.putExtra("day", days.getString(j));
+					}
+
+					PendingIntent sender = PendingIntent.getBroadcast(cordova.getActivity().getApplicationContext(), 19999 + daysOfWeek.get(days.getString(j)), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+					AlarmManager alarmManager = (AlarmManager) cordova.getActivity().getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+					if (Build.VERSION.SDK_INT>=19) {
+						alarmManager.setExact(AlarmManager.RTC_WAKEUP, alarmDate.getTimeInMillis(), sender);
+					} else {
+						alarmManager.set(AlarmManager.RTC_WAKEUP, alarmDate.getTimeInMillis(), sender);
+					}
+				}
+			}
+		}
+	}
 
 
-  protected void cancelAlarms(){
-    Log.d(LOG_TAG, "canceling alarms");
-    for (int i=0;i<7;i++){
-	    Intent intent = new Intent(cordova.getActivity().getApplicationContext(), WakeupReceiver.class);
-	    PendingIntent sender = PendingIntent.getBroadcast(cordova.getActivity().getApplicationContext(), 19999+i, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-	    AlarmManager alarmManager = (AlarmManager) cordova.getActivity().getApplicationContext().getSystemService(Context.ALARM_SERVICE);
-	    alarmManager.cancel(sender);
-    }
-  }
-  
-  protected Calendar getAlarmDate( JSONObject time, int dayOfWeek) throws JSONException {
-      TimeZone defaultz = TimeZone.getDefault();
-      Calendar calendar = new GregorianCalendar(defaultz);
-      //Date now = new Date();
-      Calendar now = new GregorianCalendar(defaultz);
-      now.setTime(new Date());
-      calendar.setTime(new Date());
+	protected void cancelAlarms(){
+		Log.d(LOG_TAG, "canceling alarms");
+		for (int i=0;i<7;i++){
+			Intent intent = new Intent(cordova.getActivity().getApplicationContext(), WakeupReceiver.class);
+			PendingIntent sender = PendingIntent.getBroadcast(cordova.getActivity().getApplicationContext(), 19999+i, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+			AlarmManager alarmManager = (AlarmManager) cordova.getActivity().getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+			alarmManager.cancel(sender);
+		}
+	}
 
-      int hour=(time.has("hour")) ? time.getInt("hour") : -1;
-      int minute=(time.has("minute")) ? time.getInt("minute") : 0;
+	protected static Calendar getAlarmDate( JSONObject time, int dayOfWeek) throws JSONException {
+		TimeZone defaultz = TimeZone.getDefault();
+		Calendar calendar = new GregorianCalendar(defaultz);
+		Calendar now = new GregorianCalendar(defaultz);
+		now.setTime(new Date());
+		calendar.setTime(new Date());
 
-      if(hour>=0){
-        calendar.set(Calendar.HOUR_OF_DAY, hour);
-        calendar.set(Calendar.MINUTE, minute);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND,0);
-        
-        int currentDayOfWeek=calendar.get(Calendar.DAY_OF_WEEK); // 1-7 = Sunday-Saturday
-        currentDayOfWeek--; // make zero-based
+		int hour=(time.has("hour")) ? time.getInt("hour") : -1;
+		int minute=(time.has("minute")) ? time.getInt("minute") : 0;
 
-        // add number of days until 'dayOfWeek' occurs
-        int daysUntilAlarm=0;
-        if(currentDayOfWeek>dayOfWeek){
-          // currentDayOfWeek=thursday (4); alarm=monday (1) -- add 4 days
-          daysUntilAlarm=(6-currentDayOfWeek) + dayOfWeek + 1; // (days until the end of week) + dayOfWeek + 1
-        }else if(currentDayOfWeek<dayOfWeek){
-          // example: currentDayOfWeek=monday (1); dayOfWeek=thursday (4) -- add three days
-          daysUntilAlarm=dayOfWeek-currentDayOfWeek;
-        }else{
-          if(now.after(calendar.getTime())){
-            daysUntilAlarm=7;
-          }else{
-            daysUntilAlarm=0;
-          }
-        }
+		if(hour>=0){
+			calendar.set(Calendar.HOUR_OF_DAY, hour);
+			calendar.set(Calendar.MINUTE, minute);
+			calendar.set(Calendar.SECOND, 0);
+			calendar.set(Calendar.MILLISECOND,0);
 
-        calendar.set(Calendar.DATE, now.get(Calendar.DATE) + daysUntilAlarm);
-      }else{
-        calendar=null;
-      }
+			int currentDayOfWeek=calendar.get(Calendar.DAY_OF_WEEK); // 1-7 = Sunday-Saturday
+			currentDayOfWeek--; // make zero-based
 
-      return calendar;
-  }
+			// add number of days until 'dayOfWeek' occurs
+			int daysUntilAlarm=0;
+			if(currentDayOfWeek>dayOfWeek){
+				// currentDayOfWeek=thursday (4); alarm=monday (1) -- add 4 days
+				daysUntilAlarm=(6-currentDayOfWeek) + dayOfWeek + 1; // (days until the end of week) + dayOfWeek + 1
+			}else if(currentDayOfWeek<dayOfWeek){
+				// example: currentDayOfWeek=monday (1); dayOfWeek=thursday (4) -- add three days
+				daysUntilAlarm=dayOfWeek-currentDayOfWeek;
+			}else{
+				if(now.after(calendar.getTime())){
+					daysUntilAlarm=7;
+				}else{
+					daysUntilAlarm=0;
+				}
+			}
+
+			calendar.set(Calendar.DATE, now.get(Calendar.DATE) + daysUntilAlarm);
+		}else{
+			calendar=null;
+		}
+
+		return calendar;
+	}
 
 }
--- src/android/WakeupReceiver.java
+++ src/android/WakeupReceiver.java
@@ -3,37 +3,77 @@ package org.nypr.cordova.wakeupplugin;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.cordova.PluginResult;
-import org.json.JSONException;
-import org.json.JSONObject;
+//import org.apache.cordova.PluginResult;
+//import org.json.JSONException;
+//import org.json.JSONObject;
 
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 public class WakeupReceiver extends BroadcastReceiver {
 
 	private static final String LOG_TAG = "WakeupReceiver";
 
+	@SuppressLint({ "SimpleDateFormat", "NewApi" })
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Log.d(LOG_TAG, "wakeuptimer expired at " + sdf.format(new Date().getTime()));
-		
+	
 		if ( WakeupPlugin.connectionCallbackContext != null ) {
-			JSONObject o=new JSONObject();
-		    PluginResult result=null;
-		    try {
-		    	o.put("type", "wakeup");
-		    	o.put("extra", intent.getExtras().get("extra"));
-		        result = new PluginResult(PluginResult.Status.OK, o);
-		    } catch (JSONException e){
-		        result = new PluginResult(PluginResult.Status.ERROR, e.getMessage());
-		    } finally {
-		    	result.setKeepCallback(true);
-		        WakeupPlugin.connectionCallbackContext.sendPluginResult(result);
-		    }
+			//JSONObject o=new JSONObject();
+			// PluginResult result=null;
+			try {
+				String packageName = context.getPackageName();
+				Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+				String className = launchIntent.getComponent().getClassName();		    	
+				Log.d(LOG_TAG, "launching activity for class " + className);
+
+				@SuppressWarnings("rawtypes")
+				Class c = Class.forName(className); 
+
+				Intent i = new Intent(context, c);
+				i.putExtra("wakeup", true);
+				i.putExtra("extra", intent.getExtras().get("extra").toString());
+				i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				context.startActivity(i);
+				//o.put("type", "wakeup");
+				//o.put("extra", intent.getExtras().get("extra"));
+				//result = new PluginResult(PluginResult.Status.OK, o);
+				// } catch (JSONException e){
+				//result = new PluginResult(PluginResult.Status.ERROR, e.getMessage());
+				
+				// repeat in one week
+				Date next = new Date(new Date().getTime() + (7 * 24 * 60 * 60 * 1000));
+				Log.d(LOG_TAG,"setting alarm at " + sdf.format(next));
+
+				Intent reschedule = new Intent(context, WakeupReceiver.class);
+				reschedule.putExtra("extra", intent.getExtras().get("extra").toString());
+				reschedule.putExtra("day", WakeupPlugin.daysOfWeek.get(intent.getExtras().get("day")));
+
+				PendingIntent sender = PendingIntent.getBroadcast(context, 19999 + WakeupPlugin.daysOfWeek.get(intent.getExtras().get("day")), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+				AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+				if (Build.VERSION.SDK_INT>=19) {
+					alarmManager.setExact(AlarmManager.RTC_WAKEUP, next.getTime(), sender);
+				} else {
+					alarmManager.set(AlarmManager.RTC_WAKEUP, next.getTime(), sender);
+				}
+				
+			} catch (ClassNotFoundException e) {
+				//result = new PluginResult(PluginResult.Status.ERROR, e.getMessage());
+				e.printStackTrace();
+			} finally {
+				//result.setKeepCallback(true);
+				//WakeupPlugin.connectionCallbackContext.sendPluginResult(result);
+				
+
+			}
 		}
 	}
 }
